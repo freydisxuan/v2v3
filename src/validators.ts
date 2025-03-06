3d5577978158275966ada6c0d4dd7a103d0ab457
freydisxuan <freydisxuan@gmail.com>
2025-03-06 20:42:56 +0000

    jdklwa

--- src/validators.ts
+++ src/validators.ts
@@ -5,7 +5,7 @@ const SlugSchema = z
   .min(1, "slug must not be empty")
   .max(1024, "slug cant be more than 1024 characters");
 
-export type Slug = z.infer<typeof SlugSchema>;
+type Slug = z.infer<typeof SlugSchema>;
 
 export function validateSlug(slug: unknown): z.SafeParseReturnType<Slug, Slug> {
   return SlugSchema.safeParse(slug);
