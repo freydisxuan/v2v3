9a9c4e9cb1185fddd9d153ea6322204c91852e44
freydisxuan <freydisxuan@gmail.com>
2025-03-06 20:37:46 +0000

    thign

--- src/validators.ts
+++ src/validators.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
-const SlugSchema = z.string().min(1, "slug must not be empty").max(1024, "slug cant be more than 1024 characters");
+const SlugSchema = z
+  .string()
+  .min(1, "slug must not be empty")
+  .max(1024, "slug cant be more than 1024 characters");
 
-type Slug = z.infer<typeof SlugSchema>;
+export type Slug = z.infer<typeof SlugSchema>;
 
 export function validateSlug(slug: unknown): z.SafeParseReturnType<Slug, Slug> {
   return SlugSchema.safeParse(slug);
